46e5e384c2b869044f9851843bb26bbba545d9e0
Jason Garber <jg@jasongarber.com>
2008-09-24 09:38:06 -0400

    Fix StandardTags redefinition.

--- redcloth4_extension.rb
+++ redcloth4_extension.rb
@@ -6,6 +6,10 @@ class Redcloth4Extension < Radiant::Extension
   description "Uses RedCloth 4 for textile_filter instead of RedCloth 3, which is bundled with Radiant."
   
   def activate
+    unless Radiant::Version::Major.to_i <= 0 && Radiant::Version::Major.to_i <= 6 && Radiant::Version::Major.to_i <= 9
+      ActionController::Base.logger.warn "This version of Radiant has built-in support for RedCloth 4.  Please remove this extension to prevent problems."
+    end
+    
     # Get the latest version of RedCloth
     Object.send :remove_const, :RedCloth if Object.const_defined?(:RedCloth) && RedCloth::VERSION.is_a?(String)
     $:.delete File.join(RADIANT_ROOT, 'vendor', 'redcloth', 'lib')
